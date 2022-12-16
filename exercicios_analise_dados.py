d314d97004126b6d44f0426c39dfeaabfcf42d82
= <=>
2022-12-15 22:44:41 -0300

    meu primeiro commit

---
+++ exercicios_analise_dados.py
@@ -0,0 +1,30 @@
+# Exercício 1 - Imprima na tela os números de 1 a 10. Use uma lista para armazenar os números.
+lista = [1,2,3,4,5,6,7,8,9,10]
+print(lista)
+# Exercício 2 - Crie uma lista de 5 objetos e imprima na tela
+lista = ['chocolate', 'banana', 'abacaxi', 'uva', 'morango']
+print(lista)
+# Exercício 3 - Crie duas strings e concatene as duas em uma terceira string
+frase1 = 'Se beber '
+frase2 = 'não dirija!'
+frase_final = frase1 + frase2
+print(frase_final)
+# Exercício 5 - Crie um dicionário vazio e imprima na tela
+dict3 = {}
+print(dict3)
+# Exercício 6 - Crie um dicionário com 3 chaves e 3 valores e imprima na tela
+dict = {'k1':'martelo', 'k2':'serrote', 'k3':'machado'}
+print(dict)
+# Exercício 8 - Crie um dicionário com 3 chaves e 3 valores. Um dos valores deve ser uma lista de 2 elementos numéricos. 
+# Imprima o dicionário na tela.
+dict2 = {'chave1':'Geografia', 'chave2':'Biologia', 'chave3':[70, 90]}
+print(dict2)
+# Exercício 9 - Crie uma lista de 4 elementos. O primeiro elemento deve ser uma string, 
+# o segundo uma tupla de 2 elementos, o terceiro um dcionário com 2 chaves e 2 valores e 
+# o quarto elemento um valor do tipo float.
+# Imprima a lista na tela.
+lst = ['String1', (90, 92), {'k1':'v1', 'k2':'v2'}, 99.98]
+print(lst)
+# Exercício 10 - Considere a string abaixo. Imprima na tela apenas os caracteres da posição 1 a 18.
+frase = 'Cientista de Dados é o profissional mais sexy do século XXI'
+frase[0:18] 
